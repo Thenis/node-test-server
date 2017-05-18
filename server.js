d20b4a5f35b741a65fc1ce98db1711663fd7b190
Thenis <d.stoilov96@gmail.com>
2017-05-18 22:29:00 +0300

    Fixed header

--- server.js
+++ server.js
@@ -48,7 +48,9 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/projects", (req, res) => {
-	res.render("projects.hbs");
+	res.render("projects.hbs", {
+		pageTitle: "Portfolio"
+	});
 });
 
 app.get("/bad", (req, res) => {
